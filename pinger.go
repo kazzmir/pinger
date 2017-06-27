4b25f7f39214e4198cf775260e2286a57bf8fdcc
Jon Rafkind <jon@headspin.io>
2017-06-27 13:56:48 -0700

    start ncurses display

--- pinger.go
+++ pinger.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 import "os"
+import "github.com/nsf/termbox-go"
 import "sync"
-import ping "github.com/sparrc/go-ping"
+import "github.com/sparrc/go-ping"
 
 func ping_host(host string){
   pinger, err := ping.NewPinger(host)
@@ -17,12 +18,40 @@ func ping_host(host string){
   fmt.Printf("Sent %d\n", stats.PacketsSent)
 }
 
+func term_print(x, y int, fg, bg termbox.Attribute, message string){
+  for _, c := range message {
+    termbox.SetCell(x, y, c, fg, bg)
+    x += 1
+  }
+}
+
+func display(hosts []string){
+  err := termbox.Init()
+  if err != nil {
+    panic(err)
+  }
+  defer termbox.Close()
+  termbox.Clear(termbox.ColorBlue, termbox.ColorBlue)
+
+  x := 1
+  y := 1
+  for _, host := range hosts {
+      term_print(x, y, termbox.ColorRed, termbox.ColorWhite, host)
+      y += 1
+  }
+  termbox.Flush()
+
+  termbox.PollEvent()
+}
+
 func main(){
   hosts := make([]string, 0)
   for _, arg := range os.Args[1:] {
     fmt.Println(arg)
     hosts = append(hosts, arg)
   }
+
+  display(hosts)
 
   var waiter sync.WaitGroup
   for _, host := range hosts {
