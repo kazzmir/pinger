6a0ef736b586d2387717af5ac845e5563e8e9609
Jon Rafkind <jon@headspin.io>
2017-06-27 13:18:23 -0700

    send all pings at once to hosts given on the command line

--- pinger.go
+++ pinger.go
@@ -1,16 +1,36 @@
 package main
 
 import "fmt"
+import "os"
+import "sync"
 import ping "github.com/sparrc/go-ping"
 
-func main(){
-  pinger, err := ping.NewPinger("www.google.com")
-  fmt.Println("hello")
+func ping_host(host string){
+  pinger, err := ping.NewPinger(host)
   if err != nil {
-    panic(err)
+    fmt.Printf("Ping failed: %s\n", err)
+    return
   }
-  pinger.Count = 3
+  pinger.Count = 1
   pinger.Run()
   stats := pinger.Statistics()
   fmt.Printf("Sent %d\n", stats.PacketsSent)
+}
+
+func main(){
+  hosts := make([]string, 0)
+  for _, arg := range os.Args[1:] {
+    fmt.Println(arg)
+    hosts = append(hosts, arg)
+  }
+
+  var waiter sync.WaitGroup
+  for _, host := range hosts {
+      waiter.Add(1)
+      go func(host string){
+          defer waiter.Done()
+          ping_host(host)
+      }(host)
+  }
+  waiter.Wait()
 }
